06dae8492b7b702d2023b663925a9cf946c3e690
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2018-04-08 19:23:49 +1000

    string split
    For get para for PLACE command, use .replaceAll("^[,\\s]+", "").split("[,\\s]+")

--- src/Appliaction.java
+++ src/Appliaction.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * Application create a Simulator object
  * for monitoring toy robot status
@@ -6,7 +7,34 @@ public class Appliaction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner reader = new Scanner(System.in);
+		Simulator simulator = new Simulator();
+		
+		//display instructions
+		showInstructions();
+		
+		boolean bExit = false;
+		while(!bExit)
+		{			
+			String str = reader.nextLine();
+			//need to validate the input string first 
+			//- but implement it later
+			
+			String[] inputs = str.replaceAll("^[,\\s]+", "").split("[,\\s]+");
+			boolean bResult = simulator.excuteCommand(inputs);
+		}
+	}
+	
+	public static void showInstructions()
+	{
+		System.out.println("========Instructions========");
+		System.out.println("PLACE X,Y,F");
+		System.out.println("MOVE");
+		System.out.println("LEFT");
+		System.out.println("RIGHT");
+		System.out.println("REPORT");
+		System.out.println("EXIT");
+		System.out.println("Please enter your command : ");
 	}
 
 }
--- src/Simulator.java
+++ src/Simulator.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 /**
  * Responsibilities:
  * Simulator reports toy robot movements on a square table top. 
@@ -11,8 +9,24 @@ public class Simulator {
 	private TableItem[][] table;
 	private ToyRobot robot;
 	
+	// constructor
+	// initial table and robot instance
 	public Simulator()
 	{
+		table = new TableItem[UNIT_SIZE][UNIT_SIZE];
+		robot = null;
+	}
+	
+	public boolean excuteCommand(String[] inputs)
+	{
+		boolean bExcuted = false;
+		if(inputs.length <= 0)
+			return bExcuted;
 		
+		for(int i=0 ; i<inputs.length ; i++)
+		{
+			System.out.println(inputs[i]);
+		}
+		return bExcuted;
 	}
 }
