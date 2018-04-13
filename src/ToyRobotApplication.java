bd2993a04f27e262f7d9457d364b7d7f54d23502
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2018-04-13 13:46:32 +1000

    add class summary for ToyRobotApplication

--- src/ToyRobotApplication.java
+++ src/ToyRobotApplication.java
@@ -6,6 +6,29 @@ import Simulator_Controller.Simulator;
  * Application create a Simulator object
  * for monitoring toy robot status
  */
+/**
+ * It is a text-based program, where a robot moves on the table top; the simulator
+ * is able to change the position, the facing information and report the current
+ * state for the user.
+ * The Table size is a 4 x 4 units. 
+ * 
+ * The Command instructions:
+ * ========Instructions========
+ * PLACE X,Y,F
+ * MOVE
+ * LEFT
+ * RIGHT
+ * REPORT
+ * EXIT
+ * Please enter your command :
+ * $>
+ * 
+ * ToyRobotApplication is where the main function is 
+ * the responsibility is that
+ * 1. show Instructions
+ * 2. get user input data for Simulator
+ * 3. display exit message after typing in exit command
+ */
 public class ToyRobotApplication {
 
 	public static void main(String[] args) {
@@ -23,6 +46,7 @@ public class ToyRobotApplication {
 				System.out.print("$>");
 				String str = reader.nextLine();
 				
+				//use regex to split parameters
 				String[] inputs = str.replaceAll("^[,\\s]+", "").split("[,\\s]+");
 				boolean bResult = simulator.excuteInput(inputs);
 				if(!bResult)
